6e6634a4e1b06588d84f2d9a93b6e3893f59a04b
Josu31506 <josuehernandezyataco@gmail.com>
2025-11-22 21:04:02 -0500

    Update api.ts

--- src/services/admin/api.ts
+++ src/services/admin/api.ts
@@ -1,5 +1,5 @@
 // src/services/admin/api.ts
-const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8081";
 
 async function request<T>(
   path: string,
